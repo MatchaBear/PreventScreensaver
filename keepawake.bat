d7d23c7d7f6c080461ec2d33d9e497f57364539b
berry <50803250+MatchaBear@users.noreply.github.com>
2022-04-16 17:28:29 +0800

    autoclose CMD window

--- keepawake.bat
+++ keepawake.bat
@@ -1,3 +1,7 @@
 
-cd C:\Users\bernhard\Downloads
-caffeine64.exe -darkicons -ontaskbar -stes
+REM cd C:\Users\bernhard\Downloads
+REM caffeine64.exe -darkicons -ontaskbar -stes
+
+REM shell:startup
+
+start C:\Users\bernhard\Downloads\caffeine64.exe -darkicons -ontaskbar -stes
